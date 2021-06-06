9c99dfbdbf23ae94ae8e164e0273c28416d5da1a
plunched <smoll.pp.exe@gmail.com>
2021-06-06 18:19:16 +0200

    added rocket

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,14 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+use rocket::*;
+
+#[get("/")]
+fn index() -> String {
+    format!("Hello, world!")
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", routes![index, ])
+        .launch();
 }
