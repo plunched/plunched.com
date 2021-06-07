c5cdd470d5a3886da8fb76bb961d5d76f16730da
plunched <smoll.pp.exe@gmail.com>
2021-06-07 17:45:33 +0200

    removed titles

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -3,7 +3,6 @@ import Layout from '../components/Layout'
 
 const AboutPage = () => (
   <Layout title="About | Next.js + TypeScript Example">
-    <h1>About</h1>
     <p>This is the about page</p>
     <p>
       <Link href="/">
